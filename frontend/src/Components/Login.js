5c90998922b49da5877fa830df779e0b36d0aa74
Gourav1100 <gauravb1100@gmail.com>
2023-04-22 02:06:24 +0530

    added tailwind css and updated login page to show login and signup separately.

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,73 +1,118 @@
-import { Button, Card, CardContent, Grid, Stack, TextField, Typography } from "@mui/material";
+import { Card, CardContent, Grid, Stack, TextField } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
-function Login(){
-    const [user, setUser] = useState("")
-    const [pass, setPass] = useState("")
+function Login() {
+    const [user, setUser] = useState("");
+    const [pass, setPass] = useState("");
+    const [isLogin, setLogin] = useState(true);
     const navigate = useNavigate();
-    useEffect(()=>{
-        if(localStorage.getItem('token')){
-            navigate('/');
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            navigate("/");
         }
     });
 
-    function login(){
-        axios.post('http://localhost:5000/login', {user: user, password: pass}).then((resp)=>{
-            if (resp.status == 200){
-                console.log(resp.data);
-                localStorage.setItem('token', resp.data);
-                localStorage.setItem('user', user);
-                navigate('/');
-            }
-            else{
-                console.log(resp.data);
-            }
-        }).catch((e)=>{
-            alert(e.response.data);
-        })
+    function login() {
+        axios
+            .post("http://localhost:5000/login", { user: user, password: pass })
+            .then((resp) => {
+                if (resp.status === 200) {
+                    localStorage.setItem("token", resp.data);
+                    localStorage.setItem("user", user);
+                    setUser("");
+                    setPass("");
+                    navigate("/");
+                }
+            })
+            .catch((e) => {
+                alert(e.response.data);
+            });
     }
 
-    function signup(){
-        axios.post('http://localhost:5000/signup', {user: user, password: pass}).then((resp)=>{
-            if (resp.status == 200){
-                console.log(resp.data);
-                alert('Registered Successfully: Please Login Now');
-            }
-            else{
-                console.log(resp.data);
-            }
-        }).catch((e)=>{
-            alert(e.response.data);
-        })
+    function signup() {
+        axios
+            .post("http://localhost:5000/signup", { user: user, password: pass })
+            .then((resp) => {
+                if (resp.status === 200) {
+                    login();
+                }
+            })
+            .catch((e) => {
+                alert(e.response.data);
+            });
     }
 
-    return(
-        <Stack height={'85vh'} sx={{mb: 0, mt:'auto', overflow: 'scroll'}} justifyContent={'center'} direction={'column'}>
+    return (
+        <Stack
+            height={"85vh"}
+            sx={{ mb: 0, mt: "auto", overflow: "scroll" }}
+            justifyContent={"center"}
+            direction={"column"}
+        >
             <Grid container>
                 <Grid item xs></Grid>
                 <Grid item xs={10} sm={5} md={4} lg={3}>
-                    <Outlet/>
-                    <Card sx={{backgroundColor: "whitesmoke"}}>
+                    <Outlet />
+                    <Card sx={{ backgroundColor: "whitesmoke" }} className="font-serif">
                         <CardContent>
-                            <Typography align="center" variant="h6">
-                                Login / Signup
-                            </Typography>
-                            <Stack mt={5}>
-                                <TextField label="Username" color={user==""?"error":"primary"} value={user} onChange={(e)=>setUser(e.target.value)} sx={{mx:2, backgroundColor:'white'}}/>
+                            <div align="center" className="pt-2 pb-14 text-2xl">
+                                {isLogin ? "Login" : "Signup"}
+                            </div>
+                            <Stack className="p-3">
+                                <TextField
+                                    label="Username"
+                                    color={user === "" ? "error" : "primary"}
+                                    value={user}
+                                    onChange={(e) => setUser(e.target.value)}
+                                    sx={{ mx: 2, backgroundColor: "white" }}
+                                />
                             </Stack>
-                            <Stack mt={4}>
-                                <TextField label="Password" type="password" color={pass==""?"error":"primary"} value={pass} onChange={(e)=>setPass(e.target.value)} sx={{mx:2, backgroundColor:'white'}}/>
+                            <Stack className="p-3">
+                                <TextField
+                                    label="Password"
+                                    type="password"
+                                    color={pass === "" ? "error" : "primary"}
+                                    value={pass}
+                                    onChange={(e) => setPass(e.target.value)}
+                                    sx={{ mx: 2, backgroundColor: "white" }}
+                                />
                             </Stack>
-                            <Grid mt={5} container>
-                                <Grid item xs={6}>
-                                    <Button onClick={login} disabled={user==""||pass==""} variant="contained"><Typography textTransform={'capitalize'}>Login</Typography></Button>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <Button onClick={signup} disabled={user==""||pass==""} variant="contained"><Typography textTransform={'capitalize'}>Signup</Typography></Button>
-                                </Grid>
-                            </Grid>
+                            <Stack className="p-3">
+                                <div className="w-full p-2 flex justify-evenly">
+                                    <div className="text-gray-600">
+                                        {isLogin
+                                            ? "Don't have an account?"
+                                            : "Already have an account?"}
+                                    </div>
+                                    <div
+                                        className="cursor-pointer text-sky-500 hover:text-sky-800"
+                                        onClick={() => setLogin(!isLogin)}
+                                    >
+                                        {isLogin ? "Signup" : "Login"}
+                                    </div>
+                                </div>
+                            </Stack>
+                            <Stack className="p-3">
+                                {isLogin ? (
+                                    <button
+                                        className="p-2 w-full rounded bg-sky-500 text-white text-lg hover:bg-sky-800"
+                                        onClick={login}
+                                        disabled={user === "" || pass === ""}
+                                    >
+                                        Login
+                                    </button>
+                                ) : (
+                                    <button
+                                        className="p-2 w-full rounded bg-sky-500 text-white text-lg hover:bg-sky-800"
+                                        onClick={signup}
+                                        disabled={user === "" || pass === ""}
+                                    >
+                                        Signup
+                                    </button>
+                                )}
+                            </Stack>
                         </CardContent>
                     </Card>
                 </Grid>

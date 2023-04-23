e8e9922afcc7ed219991943fd4312195a99833ce
shisuimadara <2020ucs0090@iitjammu.ac.in>
2023-04-24 03:01:35 +0530

    added profile update

--- backend/API/changepass.js
+++ backend/API/changepass.js
@@ -1,36 +1,53 @@
-var MongoClient = require("mongodb").MongoClient;
+var MongoClient = require("mongodb").MongoClient
+const bcrypt = require('bcrypt')
+
 var url =
     "mongodb://default:76VdQ34wZzBtt3MY@ac-qvcvywt-shard-00-00.lwxcedr.mongodb.net:27017,ac-qvcvywt-shard-00-01.lwxcedr.mongodb.net:27017,ac-qvcvywt-shard-00-02.lwxcedr.mongodb.net:27017/?ssl=true&replicaSet=atlas-vjhen2-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 async function change(req, res) {
-    //console.log("ddd");
-    if (!req.body.newpass || !req.body.currpass || !req.user) {
-        res.status(400).send("Fields Missing");
-        return;
-    }
+
     const client = await MongoClient.connect(url, { useNewUrlParser: true }).catch((err) => {
-        console.log(err);
+        console.log(err)
     });
 
     if (!client) {
         return;
     }
-    let db = client.db("seapod");
-    let col = db.collection("users");
+    let db = client.db("seapod")
+    let col = db.collection("users")
 
-    let usersp = await col.findOne({ user: req.user, password: req.body.currpass });
-    if (!usersp) {
-        client.close()
-        res.status(401).send("Current Password Mismatched");
-        return;
+    var updated_user 
+
+    if (req.body.newPassword) {
+        
+        var passHash = bcrypt.hash(req.body.password, 10)
+        const match = await bcrypt.compare(req.body.password, passHash)
+
+        if(!match) {
+            client.close()
+            res.send({
+                success: false,
+                msg: "Current password mismatched"
+            })
+            return 
+        }
+
+        passHash = bcrypt.hash(req.body.newPassword, 10)
+
+        updated_user = await col.updateOne({ user: req.user }, { $set: { password: passHash} })
     }
-    let users = await col.updateOne({ user: req.user }, { $set: { password: req.body.newpass } });
-    if (users.acknowledged) {
-        client.close()
-        res.status(200).send("Password Changed");
-    } else {
-        client.close()
-        res.sendStatus(500);
-    }
+    
+    if (req.body.newGenre) {
+
+        updated_user = await col.updateOne({user: req.user}, {$set: {genre: req.body.newGenre}})
+    }   
+    
+    client.close()
+
+    res.send({
+        success: true,
+        data: "User updated"
+    })
+    
 }
 exports.x = change;

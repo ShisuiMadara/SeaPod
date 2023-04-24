874f1a5955480781993e82dfe45861362affe56b
Gourav1100 <gauravb1100@gmail.com>
2023-04-24 14:09:12 +0530

    fixed password api

--- backend/API/changepass.js
+++ backend/API/changepass.js
@@ -1,58 +1,55 @@
-var MongoClient = require("mongodb").MongoClient
-const bcrypt = require('bcrypt')
+var MongoClient = require("mongodb").MongoClient;
+const bcrypt = require("bcrypt");
 var ObjectID = require("mongodb").ObjectId;
 
 var url =
     "mongodb://default:76VdQ34wZzBtt3MY@ac-qvcvywt-shard-00-00.lwxcedr.mongodb.net:27017,ac-qvcvywt-shard-00-01.lwxcedr.mongodb.net:27017,ac-qvcvywt-shard-00-02.lwxcedr.mongodb.net:27017/?ssl=true&replicaSet=atlas-vjhen2-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 async function change(req, res) {
-
     const client = await MongoClient.connect(url, { useNewUrlParser: true }).catch((err) => {
-        console.log(err)
+        console.log(err);
     });
 
     if (!client) {
         return;
     }
-    let db = client.db("seapod")
-    let col = db.collection("users")
-    const user = await col.findOne({ _id: new ObjectID(req.userId) })
-
-  
-    let currPassHash = user.passHash
-    var updated_user 
-    console.log(0)
+    let db = client.db("seapod");
+    let col = db.collection("user");
+    const user = await col.findOne({ userId: req.userId });
+    let currPassHash = user.passHash;
+    var updated_user;
     if (req.body.newPassword) {
-        console.log(1)
-        var PassHashProvided = await bcrypt.hash(req.body.password, 10)
-        console.log(2)
-        const match = await bcrypt.compare(currPassHash, await PassHashProvided)
+        console.log(1);
+        var PassHashProvided = await bcrypt.hash(req.body.password, 10);
+        console.log(2);
+        const match = await bcrypt.compare(currPassHash, await PassHashProvided);
 
-        if(!match) {
-            client.close()
+        if (!match) {
+            client.close();
             res.send({
                 success: false,
-                msg: "Current password mismatched"
-            })
-            return 
+                msg: "Current password mismatched",
+            });
+            return;
         }
 
-        var newPassHash = bcrypt.hash(req.body.newPassword, 10)
+        var newPassHash = bcrypt.hash(req.body.newPassword, 10);
 
-        updated_user = await col.updateOne({ user: req.user }, { $set: { password: passHash} })
+        updated_user = await col.updateOne({ user: req.user }, { $set: { password: passHash } });
     }
-    
-    if (req.body.newGenre) {
 
-        updated_user = await col.updateOne({user: req.userId}, {$set: {genre: req.body.newGenre}})
-    }   
-    
-    client.close()
+    if (req.body.newGenre) {
+        updated_user = await col.updateOne(
+            { user: req.userId },
+            { $set: { genre: req.body.newGenre } },
+        );
+    }
+
+    client.close();
 
     res.send({
         success: true,
-        data: "User updated"
-    })
-    
+        data: "User updated",
+    });
 }
 exports.x = change;
--- frontend/src/Components/Account.js
+++ frontend/src/Components/Account.js
@@ -13,16 +13,16 @@ import { Divider, Typography, Button } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
-import jwt from "jwt-decode"
+import jwt from "jwt-decode";
 
-function Account(){
+function Account() {
     const navigate = useNavigate();
-    useEffect(()=>{
-        if (!localStorage.getItem('token')){
-            navigate('/login/redir');
+    useEffect(() => {
+        if (!localStorage.getItem("token")) {
+            navigate("/login/redir");
             return;
         }
-    },[]);
+    }, []);
     const [curpass, setCurpass] = useState("");
     const [newpass, setNewpass] = useState("");
     const [newgenre, setnewGenre] = useState([]);
@@ -30,68 +30,81 @@ function Account(){
     const passwordRegex =
         /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-    function changePass(){
-        
-        console.log(jwt(localStorage.getItem("token")))
-        axios.post('http://localhost:5000/changepass', {userId: jwt(localStorage.getItem("token"))._id, password:curpass, newPassword: newpass, newGenre: newgenre}, {headers:{Authorization: `bearer ${localStorage.getItem("token")}`}}).then((resp)=>{
-            if (resp.data.success){
-                alert('Password Changed! Please Re-Login');
-                navigate('/logout');
-                window.location.href = '/'
-            }
-
-            
-        }).catch((e)=>{
-            alert(e);
-        })
-
-        
+    function changePass() {
+        console.log(jwt(localStorage.getItem("token")));
+        axios
+            .post(
+                "http://localhost:5000/changepass",
+                { password: curpass, newPassword: newpass, newGenre: newgenre },
+                { headers: { Authorization: `bearer ${localStorage.getItem("token")}` } },
+            )
+            .then((resp) => {
+                if (resp.data.success) {
+                    alert("Password Changed! Please Re-Login");
+                    navigate("/logout");
+                    window.location.href = "/";
+                }
+            })
+            .catch((e) => {
+                alert(e);
+            });
     }
-    return(
-    <Stack height={'85vh'} sx={{mb: 0, mt:'auto', overflow: 'scroll'}} justifyContent={'center'} direction={'column'}>
+    return (
+        <Stack
+            height={"85vh"}
+            sx={{ mb: 0, mt: "auto", overflow: "scroll" }}
+            justifyContent={"center"}
+            direction={"column"}
+        >
             <Grid container>
                 <Grid item xs></Grid>
                 <Grid item xs={10} sm={5} md={4} lg={3}>
-                    <Outlet/>
-                    <Card sx={{backgroundColor: "whitesmoke"}}>
+                    <Outlet />
+                    <Card sx={{ backgroundColor: "whitesmoke" }}>
                         <CardContent>
                             <Typography align="center" variant="h6">
                                 Profile
                             </Typography>
                             <Stack mt={5}>
-                                <TextField label="Username" color="success" disabled value={localStorage.getItem('user')} sx={{mx:2, backgroundColor:'white'}}/>
+                                <TextField
+                                    label="Username"
+                                    color="success"
+                                    disabled
+                                    value={localStorage.getItem("user")}
+                                    sx={{ mx: 2, backgroundColor: "white" }}
+                                />
                             </Stack>
-                            <Divider/>
-                            <Typography align="center" variant="h6" sx={{mt: 2}}>
+                            <Divider />
+                            <Typography align="center" variant="h6" sx={{ mt: 2 }}>
                                 Change Password
-                            </Typography>                                                            
+                            </Typography>
                             <Stack className="p-3">
-                                    <TextField
-                                        label="Old Password"
-                                        type="password"
-                                        color={isPasswordValid === false ? "error" : "primary"}
-                                        value={curpass}
-                                        onChange={(e) => {
-                                            setCurpass(e.target.value);
-                                            if (passwordRegex.test(e.target.value)) checkPass(true);
-                                            else checkPass(false);
-                                        }}
-                                        sx={{ mx: 2, backgroundColor: "white" }}
-                                    />
+                                <TextField
+                                    label="Old Password"
+                                    type="password"
+                                    color={isPasswordValid === false ? "error" : "primary"}
+                                    value={curpass}
+                                    onChange={(e) => {
+                                        setCurpass(e.target.value);
+                                        if (passwordRegex.test(e.target.value)) checkPass(true);
+                                        else checkPass(false);
+                                    }}
+                                    sx={{ mx: 2, backgroundColor: "white" }}
+                                />
                             </Stack>
                             <Stack className="p-3">
-                                    <TextField
-                                        label="New Password"
-                                        type="password"
-                                        color={isPasswordValid === false ? "error" : "primary"}
-                                        value={newpass}
-                                        onChange={(e) => {
-                                            setNewpass(e.target.value);
-                                            if (passwordRegex.test(e.target.value)) checkPass(true);
-                                            else checkPass(false);
-                                        }}
-                                        sx={{ mx: 2, backgroundColor: "white" }}
-                                    />
+                                <TextField
+                                    label="New Password"
+                                    type="password"
+                                    color={isPasswordValid === false ? "error" : "primary"}
+                                    value={newpass}
+                                    onChange={(e) => {
+                                        setNewpass(e.target.value);
+                                        if (passwordRegex.test(e.target.value)) checkPass(true);
+                                        else checkPass(false);
+                                    }}
+                                    sx={{ mx: 2, backgroundColor: "white" }}
+                                />
                             </Stack>
 
                             <Stack className="p-3">
@@ -104,9 +117,7 @@ function Account(){
                                         sx={{ mx: 2, backgroundColor: "white" }}
                                         multiple={true}
                                     >
-                                        <MenuItem value={"Educational"}>
-                                            Educational
-                                        </MenuItem>
+                                        <MenuItem value={"Educational"}>Educational</MenuItem>
                                         <MenuItem value={"Devotional"}>Devotional</MenuItem>
                                         <MenuItem value={"Adventure"}>Adventure</MenuItem>
                                         <MenuItem value={"Interview"}>Interview</MenuItem>
@@ -114,10 +125,18 @@ function Account(){
                                     </Select>
                                 </FormControl>
                             </Stack>
-                                
-                            <Grid mt={5} container>                                
+
+                            <Grid mt={5} container>
                                 <Grid item xs={12}>
-                                    <Button onClick={changePass} disabled={curpass==""} variant="contained"><Typography textTransform={'capitalize'}>Update Profile</Typography></Button>
+                                    <Button
+                                        onClick={changePass}
+                                        disabled={curpass == ""}
+                                        variant="contained"
+                                    >
+                                        <Typography textTransform={"capitalize"}>
+                                            Update Profile
+                                        </Typography>
+                                    </Button>
                                 </Grid>
                             </Grid>
                         </CardContent>
